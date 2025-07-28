8669c545fbfa57a6f581650c81d74d42f2ebb668
leo hancock <leohancock2002@gmail.com>
2025-07-28 10:15:38 +0800

    feat: support llama

--- nanovllm/engine/model_runner.py
+++ nanovllm/engine/model_runner.py
@@ -8,6 +8,7 @@ from nanovllm.config import Config
 from nanovllm.engine.sequence import Sequence
 from nanovllm.models.qwen3 import Qwen3ForCausalLM
 from nanovllm.models.qwen2 import Qwen2ForCausalLM
+from nanovllm.models.llama import LlamaForCausalLM
 from nanovllm.layers.sampler import Sampler
 from nanovllm.utils.context import set_context, get_context, reset_context
 from nanovllm.utils.loader import load_model
@@ -15,6 +16,7 @@ from nanovllm.utils.loader import load_model
 
 class ModelRunner:
     model_dict = {
+        "llama": LlamaForCausalLM,
         "qwen2": Qwen2ForCausalLM,
         "qwen3": Qwen3ForCausalLM,
     }
